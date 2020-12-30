ebedfa955fdd161c7c832bfb7caf39bb6eeeb340
basbeu <bastien.beuchat@epfl.ch>
2020-12-30 22:19:15 +0100

    Hungarian algo : init matrix

--- drone/mapping/hungarian_test.go
+++ drone/mapping/hungarian_test.go
@@ -3,9 +3,30 @@ package mapping
 import (
 	"testing"
 
+	"gonum.org/v1/gonum/mat"
+
 	"github.com/stretchr/testify/require"
+	"gonum.org/v1/gonum/spatial/r3"
 )
 
 func TestInitMatrix(t *testing.T) {
-	require.True(t, true)
+	mapper := NewHungarianMapper()
+
+	initialPos := []r3.Vec{
+		r3.Vec{1, 0, 0},
+		r3.Vec{2, 0, 0},
+	}
+	targetPos := []r3.Vec{
+		r3.Vec{3, 0, 0},
+		r3.Vec{4, 0, 0},
+	}
+
+	expectedMatrix := mat.NewDense(2, 2, []float64{
+		2, 3,
+		1, 2,
+	})
+
+	matrix := mapper.initMatrix(initialPos, targetPos)
+
+	require.True(t, mat.Equal(expectedMatrix, matrix))
 }
--- drone/mapping/mapper.go
+++ drone/mapping/mapper.go
@@ -1,6 +1,8 @@
 package mapping
 
 import (
+	"math"
+
 	"gonum.org/v1/gonum/mat"
 	"gonum.org/v1/gonum/spatial/r3"
 )
@@ -20,11 +22,24 @@ func NewHungarianMapper() *hungarianMapper {
 }
 
 func (m *hungarianMapper) MapTargets(initials []r3.Vec, targets []r3.Vec) map[string]r3.Vec {
+	if len(initials) != len(targets) {
+		panic("Number of drones not equal to number of targets")
+	}
+
 	//TODO
 	return make(map[string]r3.Vec)
 }
 
 func (m *hungarianMapper) initMatrix(initials []r3.Vec, targets []r3.Vec) *mat.Dense {
+	n := len(initials)
+	matrix := mat.NewDense(n, n, nil)
 
-	return nil
+	for i, drone := range initials {
+		for j, target := range targets {
+			dist := r3.Norm(drone.Sub(target))
+			matrix.Set(i, j, math.Floor(dist))
+		}
+	}
+
+	return matrix
 }
