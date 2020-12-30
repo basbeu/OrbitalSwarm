9e04e0000c353accb1e89e74114d0e178ee3aca1
basbeu <bastien.beuchat@epfl.ch>
2020-12-30 22:33:33 +0100

    Testcase initMatrix

--- drone/mapping/hungarian_test.go
+++ drone/mapping/hungarian_test.go
@@ -13,12 +13,12 @@ func TestInitMatrix(t *testing.T) {
 	mapper := NewHungarianMapper()
 
 	initialPos := []r3.Vec{
-		r3.Vec{1, 0, 0},
-		r3.Vec{2, 0, 0},
+		r3.Vec{X: 1, Y: 0, Z: 0},
+		r3.Vec{X: 2, Y: 0, Z: 0},
 	}
 	targetPos := []r3.Vec{
-		r3.Vec{3, 0, 0},
-		r3.Vec{4, 0, 0},
+		r3.Vec{X: 3, Y: 0, Z: 0},
+		r3.Vec{X: 4, Y: 0, Z: 0},
 	}
 
 	expectedMatrix := mat.NewDense(2, 2, []float64{
@@ -27,6 +27,27 @@ func TestInitMatrix(t *testing.T) {
 	})
 
 	matrix := mapper.initMatrix(initialPos, targetPos)
+
+	require.True(t, mat.Equal(expectedMatrix, matrix))
+
+	initialPos = []r3.Vec{
+		r3.Vec{X: 1, Y: 0, Z: 0},
+		r3.Vec{X: 0, Y: 2, Z: 0},
+		r3.Vec{X: 0, Y: 0, Z: 3},
+	}
+	targetPos = []r3.Vec{
+		r3.Vec{X: 1, Y: 1, Z: 1},
+		r3.Vec{X: 2, Y: 2, Z: 2},
+		r3.Vec{X: 3, Y: 3, Z: 3},
+	}
+
+	expectedMatrix = mat.NewDense(3, 3, []float64{
+		1, 3, 4,
+		1, 2, 4,
+		2, 3, 4,
+	})
+
+	matrix = mapper.initMatrix(initialPos, targetPos)
 
 	require.True(t, mat.Equal(expectedMatrix, matrix))
 }
